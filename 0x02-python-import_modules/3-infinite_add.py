0984cd73ac388630e935b71e45a5439931be311e
Tecno-wiz <st.andrewboye@yahoo.com>
2022-12-01 19:22:29 -0800

    Infinite addition

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     from sys import argv
-
-    num = len(argv)
-    result = 0
-    if num > 1:
-        for i in range(1, num):
-            result += int(argv[i])
-            print("{:d}".format(result))
+    length = len(argv)
+    sum = 0
+    for num in range(1, length):
+        sum += int(argv[num])
+        print(sum)
