66de1800d688445bb105710df2083ec81edf7242
Tecno-wiz <st.andrewboye@yahoo.com>
2023-01-11 02:28:15 -0800

    Append to a file

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -4,7 +4,7 @@ Append to a file
 """
 
 
-def append_write(filename="", text="")
+def append_write(filename="", text=""):
     """
     Appends a string at the end of a text file and returns
     the number of characters added
