4a6ed7cc92d3f070adf83989f6e52d52813fcfb3
Andrew Adeyeye <st.andrewboye@yahoo.com>
2023-04-13 13:13:03 +0100

    Update 13-model_state_delete_a.py

--- 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
-"""lists all City objects from the database hbtn_0e_101_usa"""
+"""deletes all State objects with a name containing
+the letter a from the database hbtn_0e_6_usa"""
 
 if __name__ == "__main__":
 
     import sys
-    from relationship_state import Base, State
-    from relationship_city import City
+    from model_state import Base, State
     from sqlalchemy import create_engine
     from sqlalchemy.orm import Session
     from sqlalchemy.schema import Table
@@ -16,6 +16,7 @@ if __name__ == "__main__":
     Base.metadata.create_all(engine)
 
     session = Session(engine)
-    for city in session.query(City).order_by(City.id).all():
-        print("{}: {} -> {}".format(city.id, city.name, city.state.name))
+    for state in session.query(State).filter(State.name.like('%a%')):
+        session.delete(state)
+    session.commit()
     session.close()
