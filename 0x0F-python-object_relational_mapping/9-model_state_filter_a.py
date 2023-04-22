a09e0d02ccbed7000edb76588620d8bdf768ff1e
Tecno-wiz <st.andrewboye@yahoo.com>
2023-04-22 10:39:25 -0700

    Contains a

--- 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -5,7 +5,7 @@ letter a from the database hbtn_0e_6_usa"""
 if __name__ == "__main__":
 
     import sys
-    from model_state import Base,
+    from model_state import Base, State
     from sqlalchemy import create_engine
     from sqlalchemy.orm import Session
 
